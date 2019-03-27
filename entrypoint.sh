dbcd30418dbbd4ac3ea2c0ac670a7e21bf6d76a6
Scandie <grizly.vl@gmail.com>
2019-03-27 15:30:52 +0200

    Update configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 TRY_LOOP="20"
 
-: "${REDIS_HOST:="localhost"}"
+: "${REDIS_HOST:="redis"}"
 : "${REDIS_PORT:="6379"}"
 : "${REDIS_PASSWORD:=""}"
 
-: "${POSTGRES_HOST:="127.0.0.1"}"
+: "${POSTGRES_HOST:="postgres"}"
 : "${POSTGRES_PORT:="5432"}"
 : "${POSTGRES_USER:="airflow"}"
 : "${POSTGRES_PASSWORD:="airflow"}"
